729c208245a78454a7d29eb689f817df33cd0119
GabrielHugo <hugogabriel1412@hotmail.com>
2026-01-31 13:06:09 +0100

    ajout de la détection sur image fixe

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import cv2
+from shapely.creation import destroy_prepared
+from sympy.printing.pretty.pretty_symbology import annotated
 from ultralytics import YOLO
 import numpy as np
 
 model = YOLO("yolov8n.pt")
 
-img = np.zeros((640, 640, 3), dtype=np.uint8)
+image_path = "images/voiture_1.jpg"
+img = cv2.imread(image_path)
 
-result = model(img)
+results = model(img)
+annotated_img = results[0].plot()
 
-print("Succès ! Tout est installé correctement.")
+cv2.imshow("Detection YOLOv8 - Hugo", annotated_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
