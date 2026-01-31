0b49e37d86600b1245abf6d021843edf6c80fd1f
GabrielHugo <hugogabriel1412@hotmail.com>
2026-01-31 15:56:45 +0100

    ajout de l'isolement des voitures détectées

--- main.py
+++ main.py
@@ -10,9 +10,21 @@ image_path = "images/voiture_1.jpg"
 img = cv2.imread(image_path)
 
 results = model(img)
-annotated_img = results[0].plot()
 
-cv2.imshow("Detection YOLOv8 - Hugo", annotated_img)
+for result in results :
+
+    boxes = result.boxes
+
+    for box in boxes :
+
+        cls = int(box.cls[0])
+
+        if cls in [2, 3, 5, 7]:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            car_crop = img[y1: y2, x1: x2]
+            cv2.imshow("Voiture isolée", car_crop)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
