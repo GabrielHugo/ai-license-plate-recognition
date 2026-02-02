043e4bf5efe38bc95735c3ca722a79cc8bdee019
GabrielHugo <hugogabriel1412@hotmail.com>
2026-02-02 17:53:47 +0100

    ajout de la détection et lecture des plaques d'immatriculation avec affichage des résultats

---
+++ README.md
@@ -0,0 +1,19 @@
+### Crédits et Licences
+
+Ce projet a été rendu possible grâce à l'utilisation de datasets open-source pour l'entraînement des modèles IA.
+
+**1. Dataset Plaques (Détection) :**
+* **Titre** : *European License Plates Dataset*
+* **Auteur** : Patrick Neicu
+* **Source** : Roboflow Universe
+* **Date** : Mars 2024
+* **Lien** : [https://universe.roboflow.com/patrick-neicu/european-license-plates](https://universe.roboflow.com/patrick-neicu/european-license-plates)
+* **Licence** : CC BY 4.0
+
+**2. Dataset Caractères (Lecture) :**
+* **Titre** : *License Plate Characters Dataset*
+* **Auteur** : Adam Toth
+* **Source** : Roboflow Universe
+* **Date** : August 2023
+* **Lien** : [https://universe.roboflow.com/adam-toth-b7suq/license-plate-characters-7qltj](https://universe.roboflow.com/adam-toth-b7suq/license-plate-characters-7qltj)
+* **Licence** : CC BY 4.0
--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 import cv2
-from shapely.creation import destroy_prepared
-from sympy.printing.pretty.pretty_symbology import annotated
 from ultralytics import YOLO
-import numpy as np
 
-model = YOLO("yolov8n.pt")
+model_plaque = YOLO("best_plaque.pt")
+model_characteres = YOLO("best_char.pt")
 
-image_path = "images/voiture_1.jpg"
-img = cv2.imread(image_path)
+images_path = "images/voiture_2.jpg"
+img = cv2.imread(images_path)
 
-results = model(img)
+results_plaque = model_plaque(img, conf=0.25)
 
-for result in results :
+plaque_trouvee = False
 
-    boxes = result.boxes
+for r in results_plaque :
+    for box in r.boxes :
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        plaque_trouvee = True
 
-    for box in boxes :
+        plate_crop = img[y1:y2, x1:x2]
 
-        cls = int(box.cls[0])
+        results_chars = model_characteres(plate_crop, conf=0.40)
 
-        if cls in [2, 3, 5, 7]:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
+        detections = []
 
-            car_crop = img[y1: y2, x1: x2]
-            cv2.imshow("Voiture isolée", car_crop)
+        for r_char in results_chars :
+            for box_char in r_char.boxes :
+                cx1, cy1, cx2, cy2 = map(int, box_char.xyxy[0])
 
+                cls_id = int(box_char.cls[0])
+                char_name = model_characteres.names[cls_id]
 
+                detections.append((cx1, char_name))
+
+                cv2.rectangle(plate_crop, (cx1, cy1), (cx2, cy2), (0, 0, 255), 1)
+
+            detections.sort(key=lambda x: x[0])
+
+            texte_final = "".join([d[1] for d in detections])
+
+            print(f"Plaque détectée : {texte_final}")
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, texte_final, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            cv2.imshow(f"Zoom plaque {texte_final}", plate_crop)
+
+if not plaque_trouvee :
+    print("Aucune plaque trouvée")
+
+cv2.imshow("Résultat Global", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
